b61fb68e0678f28213d9f1b6605c34078956fbad
mehrdad <mehr.3049@gmail.com>
2019-01-17 17:27:37 +0330

    add Data

--- src/main/java/com/sadad/jib/ws/config/dto/AccountTransactionsResponseFeign.java
+++ src/main/java/com/sadad/jib/ws/config/dto/AccountTransactionsResponseFeign.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 import java.util.List;
 
-
+@Data
 public class AccountTransactionsResponseFeign {
 
     private Integer totalPages;
@@ -20,99 +20,5 @@ public class AccountTransactionsResponseFeign {
     private Boolean previous;
     private List<ContentResponseFeign> content;
 
-    public Integer getTotalPages() {
-        return totalPages;
-    }
 
-    public void setTotalPages(Integer totalPages) {
-        this.totalPages = totalPages;
-    }
-
-    public Integer getTotalElements() {
-        return totalElements;
-    }
-
-    public void setTotalElements(Integer totalElements) {
-        this.totalElements = totalElements;
-    }
-
-    public Integer getNumberOfElements() {
-        return numberOfElements;
-    }
-
-    public void setNumberOfElements(Integer numberOfElements) {
-        this.numberOfElements = numberOfElements;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
-
-    public void setNumber(Integer number) {
-        this.number = number;
-    }
-
-    public Integer getSize() {
-        return size;
-    }
-
-    public void setSize(Integer size) {
-        this.size = size;
-    }
-
-    public Boolean getHasContent() {
-        return hasContent;
-    }
-
-    public void setHasContent(Boolean hasContent) {
-        this.hasContent = hasContent;
-    }
-
-    public Object getSort() {
-        return sort;
-    }
-
-    public void setSort(Object sort) {
-        this.sort = sort;
-    }
-
-    public Boolean getFirst() {
-        return first;
-    }
-
-    public void setFirst(Boolean first) {
-        this.first = first;
-    }
-
-    public Boolean getLast() {
-        return last;
-    }
-
-    public void setLast(Boolean last) {
-        this.last = last;
-    }
-
-    public Boolean getNext() {
-        return next;
-    }
-
-    public void setNext(Boolean next) {
-        this.next = next;
-    }
-
-    public Boolean getPrevious() {
-        return previous;
-    }
-
-    public void setPrevious(Boolean previous) {
-        this.previous = previous;
-    }
-
-    public List<ContentResponseFeign> getContent() {
-        return content;
-    }
-
-    public void setContent(List<ContentResponseFeign> content) {
-        this.content = content;
-    }
 }
--- src/main/java/com/sadad/jib/ws/config/dto/ContentResponseFeign.java
+++ src/main/java/com/sadad/jib/ws/config/dto/ContentResponseFeign.java
@@ -2,7 +2,7 @@ package com.sadad.jib.ws.config.dto;
 
 import lombok.Data;
 
-
+@Data
 public class ContentResponseFeign {
 
     private Integer transactionId;
@@ -17,92 +17,4 @@ public class ContentResponseFeign {
     private String transactionChannel;
     private Integer terminalId;
 
-
-    public Integer getTransactionId() {
-        return transactionId;
-    }
-
-    public void setTransactionId(Integer transactionId) {
-        this.transactionId = transactionId;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public String getTransactionDateTime() {
-        return transactionDateTime;
-    }
-
-    public void setTransactionDateTime(String transactionDateTime) {
-        this.transactionDateTime = transactionDateTime;
-    }
-
-    public Integer getBranchCode() {
-        return branchCode;
-    }
-
-    public void setBranchCode(Integer branchCode) {
-        this.branchCode = branchCode;
-    }
-
-    public String getBranchName() {
-        return branchName;
-    }
-
-    public void setBranchName(String branchName) {
-        this.branchName = branchName;
-    }
-
-    public String getDebitCredit() {
-        return debitCredit;
-    }
-
-    public void setDebitCredit(String debitCredit) {
-        this.debitCredit = debitCredit;
-    }
-
-    public String getTraceNumber() {
-        return traceNumber;
-    }
-
-    public void setTraceNumber(String traceNumber) {
-        this.traceNumber = traceNumber;
-    }
-
-    public String getTransactionDescription() {
-        return transactionDescription;
-    }
-
-    public void setTransactionDescription(String transactionDescription) {
-        this.transactionDescription = transactionDescription;
-    }
-
-    public Integer getTransactionAmount() {
-        return transactionAmount;
-    }
-
-    public void setTransactionAmount(Integer transactionAmount) {
-        this.transactionAmount = transactionAmount;
-    }
-
-    public String getTransactionChannel() {
-        return transactionChannel;
-    }
-
-    public void setTransactionChannel(String transactionChannel) {
-        this.transactionChannel = transactionChannel;
-    }
-
-    public Integer getTerminalId() {
-        return terminalId;
-    }
-
-    public void setTerminalId(Integer terminalId) {
-        this.terminalId = terminalId;
-    }
 }
