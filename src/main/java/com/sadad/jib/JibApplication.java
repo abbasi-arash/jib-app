f8f708b879071f76bcdb582c9444ca2b2cc3ef59
mehrdad <mehr.3049@gmail.com>
2019-01-17 14:50:23 +0330

    add token filter

--- src/main/java/com/sadad/jib/JibApplication.java
+++ src/main/java/com/sadad/jib/JibApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableFeignClients
 public class JibApplication {
-	private static final Logger log = LoggerFactory.getLogger(JibApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(JibApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(JibApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JibApplication.class, args);
+    }
 }
